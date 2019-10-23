2c905c026bab774dae1c2241b8a9035705583b5f
Brandi <blubrandi0@gmail.com>
2019-10-23 13:26:45 -0600

    Added Usernotifications and FoodMinder model

--- foodMinder/foodMinder/AddFoodMinderViewController.swift
+++ foodMinder/foodMinder/AddFoodMinderViewController.swift
@@ -9,7 +9,56 @@
 import UIKit
 import UserNotifications
 
+protocol AddFoodMinderDelegate {
+    func foodMinderWasAdded(_ foodMinder: FoodMinder)
+}
+
 class AddFoodMinderViewController: UIViewController {
+    
+    @IBOutlet weak var foodMinderTitle: UITextField!
+    @IBOutlet weak var foodMinderDatePicker: UIDatePicker!
+    
+    @IBAction func saveFoodMinderTapped(_ sender: UIButton) {
+        
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+                DispatchQueue.main.async {
+                if settings.authorizationStatus == UNAuthorizationStatus.authorized {
+                    let time = self.foodMinderDatePicker.date.timeIntervalSinceNow
+                    
+                    if time > 0 {
+                        
+                        let notificationContent = UNMutableNotificationContent()
+                        guard let foodMinderTitle = self.foodMinderTitle.text else { return }
+                        
+                        notificationContent.body = foodMinderTitle
+                        notificationContent.title = "foodMinder"
+                        notificationContent.sound = UNNotificationSound.default
+                        
+                        
+                        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: time, repeats: false)
+                        let request = UNNotificationRequest(identifier: "foodMinder", content: notificationContent, trigger: trigger)
+                        
+                        let foodMinder = FoodMinder(title: notificationContent.title, body: notificationContent.body, completed: false)
+                        self.delegate?.foodMinderWasAdded(foodMinder)
+                        
+                        UNUserNotificationCenter.current().add(request) { (_) in
+                            DispatchQueue.main.async {
+                                let alert = UIAlertController(title: "Suhweet!", message: "You set a foodMinder to eat!", preferredStyle: .alert)
+                                let close = UIAlertAction(title: "Close", style: .default)
+                                alert.addAction(close)
+                                self.present(alert, animated: true)
+                                
+                                
+                            }
+                        }
+                        
+                    }
+                }
+                }
+            }
+        }
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ foodMinder/foodMinder/FoodMinder.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMinder.swift
+//  foodMinder
+//
+//  Created by Brandi on 10/23/19.
+//  Copyright © 2019 Brandi. All rights reserved.
+//
+
+import Foundation
+
+struct FoodMinder: Codable {
+    
+    let title: String
+    let body: String
+    var completed: Bool
+    
+}
